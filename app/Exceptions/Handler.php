a1666bdd7f003539b43025a267d8f24681f9896a
paoloconza <paolocoha@gmail.com>
2024-01-14 22:54:46 -0500

    api de auth terminada

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -17,6 +18,23 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        if ($e->response) {
+            return $e->response;
+        }
+
+        return $this->invalidJson($request, $e);
+    }
+
+    public function invalidJson($request, ValidationException $e)
+    {
+        return response()->json([
+            'message' => $e->getMessage(),
+            'errors' => $e->errors(),
+        ], $e->status);
+    }
 
     /**
      * Register the exception handling callbacks for the application.
