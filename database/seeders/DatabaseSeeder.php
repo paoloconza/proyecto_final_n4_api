17bd4d1df4c118688f6ab351478af33bb2a7bd17
paoloconza <paolocoha@gmail.com>
2024-01-18 00:32:46 -0500

    api para las tablas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+
+        $this->call([
+            RoleSeeder::class,
+            EnlaceSeeder::class,
+        ]);
+        User::factory(10)->create();
+
+        $this->call(BitacoraSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('roles')->insert([
+            'nombre' => 'admin',
+        ]);
+        DB::table('roles')->insert([
+            'nombre' => 'maestro',
+        ]);
+        DB::table('roles')->insert([
+            'nombre' => 'alumno',
+        ]);
+    }
+}
